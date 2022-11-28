48b18374c2e17eda34b352f7201df1dfcdf67aba
soxhwhat <1246669436@qq.com>
2022-11-28 22:04:35 +0800

    feature：更换终端oh-my-zsh

--- cpu-api/p4.c
+++ cpu-api/p4.c
@@ -21,7 +21,7 @@ main(int argc, char *argv[])
 
 	// now exec "wc"...
         char *myargs[3];
-        myargs[0] = strdup("wc");   // program: "wc" (word count)
+        myargs[0] = strdup("wc");   ""// program: "wc" (word count)
         myargs[1] = strdup("Makefile"); // argument: file to count
         myargs[2] = NULL;           // marks end of array
         execvp(myargs[0], myargs);  // runs word count
